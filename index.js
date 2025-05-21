23e5a263987449bf9da241d92f443e465df0fcea
Kyle Housel <kylegranthousel@gmail.com>
2025-05-21 09:08:05 -0400

    Write palindrome functions in working state

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+reverseString = (string) => {
+  return string.split('').reverse().join('')
 }
 
-/* 
-  Add your pseudocode here
+isPalindrome = (word) => {
+  return word === reverseString(word)
+}
+
+/*
+  1. Create a helper function that reverses a given string, takes in a string and returns a string
+  2. Create function to evaluate palindrome that takes in a string and returns a bool
+    a. if string === reversedString return true else return false
 */
 
 /*
-  Add written explanation of your solution here
+  A palindrome is a word or phrase that when spelled backwards, appears the same as it does spelled normally.
+  Looking at the test cases, the input will be a string (the word to be checked whether or not it is a palindrome),
+  and the output is a boolean (return true if palindrome, false if not).
+  To check for palindromes, my thought process is to store each string in some way, reverse the string and store that
+  value as well, then compare if the two are strictly equal. If so, it is indeed a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
